71f461a583187f70fbbf5f070bc62f55bb32f8b3
datochan168 <lambomiura@gmail.com>
2020-02-18 10:00:13 +0800

    Baseline commit

---
+++ twsolid/twtest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace twtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
